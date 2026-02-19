f5385f71cd33345b3a7955c6179cdf22b6ce9181
Vic-Nas <nasci.victorio@gmail.com>
2026-02-18 19:10:02 -0500

    Add cookie settings

--- project/settings.py
+++ project/settings.py
@@ -17,6 +17,11 @@ else:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
     CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
 
+# Security settings
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
