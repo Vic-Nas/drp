1089423da182a4980ee2b8d095439ceba9426b08
Vic-Nas <nasci.victorio@gmail.com>
2026-02-18 19:48:50 -0500

    fix: add SECURE_PROXY_SSL_HEADER to resolve CSRF 403 behind reverse proxy

--- project/settings.py
+++ project/settings.py
@@ -13,16 +13,12 @@ DOMAIN = os.environ.get('DOMAIN')
 if DOMAIN:
     ALLOWED_HOSTS = [DOMAIN]
     CSRF_TRUSTED_ORIGINS = [f'https://{DOMAIN}', f'http://{DOMAIN}']
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
 else:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
     CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
-
-# Security settings
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SAMESITE = 'Lax'
-CSRF_COOKIE_SAMESITE = 'Lax'
-CSRF_USE_SESSIONS = False
 
 INSTALLED_APPS = [
     'django.contrib.admin',
