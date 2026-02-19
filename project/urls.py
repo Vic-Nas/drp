aac34632c258776ddbf3acd9b4d5913c6ae431bf
Vic-Nas <nasci.victorio@gmail.com>
2026-02-19 09:38:27 -0500

    Add crash report

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views
 
+handler500 = 'core.views.error_handler.server_error'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
